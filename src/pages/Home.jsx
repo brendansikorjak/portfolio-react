319c076a6decf604e5f4e7f4ed3bc9a2b0f09b3f
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-26 16:20:06 -0400

    created basic about me page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
-import { Home } from '@mui/icons-material';
+import { Box } from '@mui/material';
+import portrait from '../assets/Brendan.jpeg';
+
+function Home() {
+  return (
+    <Box>
+      <h1> Brendan Sikorjak </h1>
+      <div>
+        <img src={portrait} id="portrait"></img>
+      </div>
+      <p>
+        My name is Brendan Sikorjak and I am a huge fan of the New York Rangers.
+        I am a first generation born American to immigrant parents from Slovakia
+        and Czechia. Playing ice hockey, snowboarding and tennis are my hobbies
+        and I was lucky enough to turn one of those sports into a profession.
+        During the spring, summer, and fall I coach tennis to children and
+        senior citizens and have a blast doing it! I am currently pursuing my
+        career in computer science at a bootcamp provided by Rutgers University
+        to build my portfolio.
+      </p>
+    </Box>
+  );
+}
 
 export default Home;
