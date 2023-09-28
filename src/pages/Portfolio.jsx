dbf0b2e0758cbae8ed5c73100d76837cf61c1c8f
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-27 21:36:40 -0400

    added rangers quiz to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,41 @@
-import { Box } from '@mui/material';
+import * as React from 'react';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from '@mui/material';
+import nyrLogo from '../assets/new-york-rangers-logo.png';
 
 function Portfolio() {
   return (
     <Box>
       <h1> Portfolio </h1>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea
+          href="https://brendansikorjak.github.io/new-york-rangers-quiz/"
+          target="_blank"
+        >
+          <CardMedia
+            component="img"
+            height="300"
+            image={nyrLogo}
+            alt="NYR Logo"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              New York Rangers quiz
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Test your knowledge on the jersey numbers of some of the players
+              for the New York Rangers!
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </Box>
   );
 }
-
 export default Portfolio;
