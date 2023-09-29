ee51224b8b4c579a993904c7ee5e917a3e50c049
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-09-29 12:45:45 -0400

    added resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,20 @@
 import { Box } from '@mui/material';
+import { Document, Page } from 'react-pdf';
+import resume from '../assets/resume.pdf'
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
 
 function Resume() {
   return (
     <Box>
       <h1> Resume </h1>
+      <Document file={resume} >
+      <Page pageNumber={1} />
+    </Document>
     </Box>
   );
 }
