3ffc2b7d8f7ab75f71da21861bc5020a16bb38b3
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-10-02 16:17:50 -0400

    added dictionary to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,20 +5,23 @@ import {
   Card,
   CardContent,
   CardMedia,
+  CardActions,
   Typography,
   CardActionArea,
 } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import nyrLogo from '../assets/new-york-rangers-logo.png';
 import nutritionLogo from '../assets/nutrition-logo.png';
 import weatherPic from '../assets/weather-app.png';
 import purrfectLogo from '../assets/purrfect-logo.svg';
+import dictionaryPic from '../assets/dictionary.jpg';
 
 function Portfolio() {
   return (
     <Box>
       <h1> Portfolio </h1>
       <Grid container spacing={2}>
-        <Card sx={{ maxWidth: 325 }}>
+        <Card sx={{ width: 325 }}>
           <CardActionArea
             href="https://brendansikorjak.github.io/new-york-rangers-quiz/"
             target="_blank"
@@ -39,8 +42,16 @@ function Portfolio() {
               </Typography>
             </CardContent>
           </CardActionArea>
+          <CardActions>
+            <a
+              href="https://github.com/brendansikorjak/new-york-rangers-quiz"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 325 }}>
+        <Card sx={{ width: 325 }}>
           <CardActionArea
             href="https://sergrojas29.github.io/Nutrition-Tracker/"
             target="_blank"
@@ -61,8 +72,16 @@ function Portfolio() {
               </Typography>
             </CardContent>
           </CardActionArea>
+          <CardActions>
+            <a
+              href="https://github.com/Sergrojas29/GroupProject1"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 325 }}>
+        <Card sx={{ width: 325 }}>
           <CardActionArea
             href="https://brendansikorjak.github.io/weather-app/"
             target="_blank"
@@ -83,8 +102,16 @@ function Portfolio() {
               </Typography>
             </CardContent>
           </CardActionArea>
+          <CardActions>
+            <a
+              href="https://github.com/brendansikorjak/weather-app"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </CardActions>
         </Card>
-        <Card sx={{ maxWidth: 325 }}>
+        <Card sx={{ width: 325 }}>
           <CardActionArea
             href="https://new-cat-cafe-748a9644a8c0.herokuapp.com/"
             target="_blank"
@@ -104,6 +131,45 @@ function Portfolio() {
               </Typography>
             </CardContent>
           </CardActionArea>
+          <CardActions>
+            <a
+              href="https://github.com/Sergrojas29/Purrfect-Cat-Cafe"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </CardActions>
+        </Card>
+        <Card sx={{ width: 325 }}>
+          <CardActionArea
+            href=" https://main.d24rgv9ivojyz8.amplifyapp.com/"
+            target="_blank"
+          >
+            <CardMedia
+              component="img"
+              height="250"
+              image={dictionaryPic}
+              alt="Dictionary App"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Dictionary App
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Get a random word with the definition or play a multiple choice
+                quiz to test your knowledge from any of the following
+                dictionaries: French, Biology, Chemistry, Geology, and Medical!
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <a
+              href="https://github.com/brendansikorjak/dictionary"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </a>
+          </CardActions>
         </Card>
       </Grid>
     </Box>
