3113f813fc4fd7c091cd06ddd2c257ad1599e2ac
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-11-22 12:45:16 -0500

    updated to 1 page resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
       <h1> Resume </h1>
       <a
         className="btn"
-        href="CS-resume.pdf"
+        href="CS-resume2.pdf"
         download="Brendan-Sikorjak-resume"
       >
         Download my Resume
